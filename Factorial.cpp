ca7ab24a1bbdba4adcc6b5b237e2c6f1e5614523
EduardoEscarzaga0228 <al215024@alumnos.uacj.mx>
2024-10-29 11:18:43 -0600

    Archivo modificado

--- Factorial.cpp
+++ Factorial.cpp
@@ -7,16 +7,61 @@ int factorial(int n) {
     else
         return n * factorial(n - 1);
 }
-int main() {
-    int numero;
-    cout << "Introduce un numero para calcular su factorial: ";
-    cin >> numero;
-    
-    if (numero < 0) {
-        cout << "El factorial no está definido para números negativos." << endl;
-    } else {
-        cout << "El factorial de " << numero << " es: " << factorial(numero) << endl;
-    }
 
+void cajeroautomatico(){
+    int saldo = 2000;
+    int x,opc, cantidad,saldo;
+
+    do{
+        cout<<endl<<"Menu"<<endl;
+        cout<<endl<<"1. Consultar saldo";
+        cout<<endl<<"2. Retirar efectivo";
+        switch (x){
+        case 1:
+            cout<<endl<<"Saldo: " << saldo;
+            break;
+        
+        case 2:
+            cout<<endl<<"Ingrese la cantidad que desea retirar: "<<endl;
+            cin>>cantidad;
+            saldo=saldo-cantidad;
+            cout<<endl<<"Su saldo actual es: "<<saldo;
+        default:
+        cout<<"Error";
+            break;
+        }
+        cout<<endl<<"Desea salir del programa? s/n"<<endl;
+        cin>>opc;
+    }while(opc=='s')
+}
+
+
+int main() {
+    int opc,y;
+    int numero;
+
+    cout<<endl<<"Menu de opciones"<<endl;
+    cout<<endl<<"1. Calcular factorial"<<endl;
+    cout<<endl<<"2. Usar cajero Automatico"<<endl;
+    cout<<endl<<"Ingresa una opcion"<<endl;
+    cin>>y;
+
+    do{
+        switch(y){
+            case 1: 
+                cout<<endl<<"Introduce un numero para calcular su factorial: "<<endl;
+                cin>>numero;
+                    if (numero<0) {
+                        cout<<endl<<"No se puede factorizar numeros negativos"<<endl;
+                    }else{
+                        cout<<endl<<"El factorial de " << numero << " es: " << factorial(numero)<<endl;
+                    }
+                break;
+
+            case2:
+                cajeroautomatico();
+                break;
+        }
+    }while(opc=='s')
     return 0;
 }
